7de70873471dc2fa0bd2df3077ee88191f19033a
amir <amirazizihosein@gmail.com>
2023-01-12 14:57:10 +0330

    project-complete

---
+++ app.js
@@ -0,0 +1,18 @@
+// setup date
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("date").innerHTML = year;
+// setup nav
+const navBtn = document.getElementById("nav-btn");
+const navbar = document.getElementById("navbar");
+const navClose = document.getElementById("nav-close");
+
+// show nav
+navBtn.addEventListener("click", () => {
+  navbar.classList.add("showNav");
+});
+
+// close nav
+navClose.addEventListener("click", () => {
+  navbar.classList.remove("showNav");
+});
